042991a541684851da03fbee4b1b7b0526df821d
Shreyash <shreyash270894@gmail.com>
2018-07-02 20:38:55 +0530

    completed

---
+++ assets/js/todos.js
@@ -0,0 +1,19 @@
+$("ul").on("click","li",function(){
+	$(this).toggleClass("completed");
+});
+$("ul").on("click","span",function(event){
+	$(this).parent().fadeOut(500,function(){
+		$(this).remove();
+	});
+	event.stopPropagation();
+});
+$("input[type='text']").keypress(function(event){
+	if (event.which === 13) {
+		var a= $(this).val();
+		$(this).val("");
+		$("ul").append("<li><span><i class='fa fa-trash'></i></span> " +a+ "</li>");
+	}
+});
+$(".fa-plus").click(function(){
+	$("input[type='text']").fadeToggle();
+});
